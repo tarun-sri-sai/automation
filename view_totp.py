dcc06c49bc16c0ae2400ee18643a54cca8c4a7b1
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-07-19 17:48:55 +0530

    refactor totp scripts

--- view_totp.py
+++ view_totp.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from rich.console import Console
-from lib.totp import get_totp_urls, build_table
+from lib.totp.parse import get_totp_urls, build_table
 
 
 def main():
@@ -18,11 +18,16 @@ def main():
         action="store_true",
         help="Whether the file is encrypted (OpenPGP)"
     )
+    parser.add_argument(
+        "-r",
+        "--recipient",
+        type=str,
+        help="recipient to use for decryption (required if file is encrypted)"
+    )
     args = parser.parse_args()
 
-    totp_urls = get_totp_urls(args.file, args.encrypted)
-
     console = Console()
+    totp_urls = get_totp_urls(args.file, args.encrypted, args.recipient)
     console.print(build_table(totp_urls, raw=True))
 
 
