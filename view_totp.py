404734e43bb8652b73dc6e624bcfd05d3eaffc12
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-09-12 22:35:38 +0530

    update view_totp script
    add support for proton_authenticator_export_v1 encryption type
    add option to print raw urls

--- view_totp.py
+++ view_totp.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from rich.console import Console
-from lib.encryption import GnupgContext
+from lib.encryption import GnupgContext, ProtonAuthenticatorExportV1Context
+from lib.encryption.core import read_password
 from lib.totp.parse import get_totp_urls, build_table
 
 
@@ -25,14 +26,30 @@ def main():
         help="gnupg recipient to use for decryption and encryption"
     )
 
+    parser.add_argument(
+        "-r",
+        "--raw",
+        action="store_true"
+    )
+
     args = parser.parse_args()
 
     ctx = None
     if args.encryption_type == "gnupg":
         ctx = GnupgContext(args.gnupg_recipient)
+    elif args.encryption_type == "proton_authenticator_export_v1":
+        password = read_password(
+            "enter password for proton authenticator export v1: "
+        )
+        ctx = ProtonAuthenticatorExportV1Context(password)
 
     console = Console()
     totp_urls = get_totp_urls(args.file, ctx)
+
+    if args.raw:
+        print("\n".join(totp_urls))
+        return
+
     console.print(build_table(totp_urls, raw=True))
 
 
